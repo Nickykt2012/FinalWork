857bed182e31a761703c6468c1bd3ffb6c56f618
Nick <Sofronov_ns2012@mail.ru>
2023-03-20 23:00:40 +0300

    Создан вывод массива

--- Programm/Program.cs
+++ Programm/Program.cs
@@ -1,8 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 string[] myArray = {"Hello", "Russia", "-2", "One"};
-for (int i = 0; i < 4; i++)
+for (int i = 0; i < myArray.Length; i++)
 {
 Console.WriteLine(myArray [i]);
 }
+
+
 
